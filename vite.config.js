bd86e63112b30c2dd46ae7f88dc251382ec62e21
sunfloweerr5 <defa05321@gmail.com>
2025-09-09 15:04:42 +0700

    1

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    port: 3000
-  }
-})
+  plugins: [tailwindcss()],
+});
